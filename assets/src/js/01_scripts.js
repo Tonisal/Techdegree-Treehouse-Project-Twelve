7ccc1760c0ad5e7ac1e1f00fc09680341a68c8b0
Tonisal <markus.bressel@gmx.de>
2018-12-16 19:54:14 +0100

    add current status

--- assets/src/js/01_scripts.js
+++ assets/src/js/01_scripts.js
@@ -1,12 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
     toggleMobileNavigation();
+    scrollToLinkPosition();
+    toggleCollapsedContent();
+    toggleNavigationMode();
 });
 
 
-
 function toggleMobileNavigation() {
-    $('.m-btn-mobile-menu').click(function() {
+    $('.m-btn-mobile-menu').click(function () {
         $(this).toggleClass('m-btn-mobile-menu--active');
         $('.m-mobile-nav').toggleClass('m-mobile-nav--active');
     })
+}
+
+function toggleNavigationMode() {
+    $(window).resize(function () {
+        if (window.innerWidth > 768) {
+            console.log('desktop');
+        }
+        ;
+    });
+}
+
+function scrollToLinkPosition(event) {
+    $('.m-main-nav a, .m-mobile-nav a').click(function (event) {
+        event.preventDefault();
+        $('.m-mobile-nav').removeClass('m-mobile-nav--active');
+        $('.m-btn-mobile-menu').removeClass('m-btn-mobile-menu--active');
+        var target = $(this).attr('href');
+        console.log(target);
+        target = ($(target).offset().top);
+        target = target - 64;
+        $('html, body').animate({
+            scrollTop: target
+        }, 'slow');
+
+
+    });
+}
+
+function toggleCollapsedContent() {
+    $('.m-collapsed-content').click(function () {
+        $(this).toggleClass('m-collapsed-content--active');
+    });
 }
