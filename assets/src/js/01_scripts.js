eed5c456f3da023e2ccdea4c7ba7f0a963549341
Tonisal <markus.bressel@gmx.de>
2018-12-22 17:16:54 +0100

    add current status

--- assets/src/js/01_scripts.js
+++ assets/src/js/01_scripts.js
@@ -3,7 +3,10 @@ $(document).ready(function () {
     scrollToLinkPosition();
     toggleCollapsedContent();
     toggleNavigationMode();
+    navigationLinkHighlighting();
 });
+
+var mainNavigationHeight = 65;
 
 
 function toggleMobileNavigation() {
@@ -17,8 +20,12 @@ function toggleNavigationMode() {
     $(window).resize(function () {
         if (window.innerWidth > 768) {
             console.log('desktop');
+            $('.m-mobile-nav').removeClass('m-mobile-nav--active');
+            $('.m-btn-mobile-menu').removeClass('m-btn-mobile-menu--active');
+        } else {
+            console.log('mobil');
         }
-        ;
+
     });
 }
 
@@ -42,5 +49,41 @@ function scrollToLinkPosition(event) {
 function toggleCollapsedContent() {
     $('.m-collapsed-content').click(function () {
         $(this).toggleClass('m-collapsed-content--active');
+    });
+}
+
+function navigationLinkHighlighting() {
+    var linksToHighlight = $('.m-main-nav li a');
+    var linkTargets = [];
+    var elementPositions = {};
+    linksToHighlight.each(function () {
+        linkTargets.push($(this).attr('href').replace('#', ''));
+    });
+
+    for(var i = 0; i < linksToHighlight.length; i++) {
+        var elementToAdd = linkTargets[i];
+        elementPositions[elementToAdd] = $('#' + linkTargets[i]).offset().top - mainNavigationHeight;
+    }
+
+    $(window).scroll(function () {
+        var currentScrollPosition = $(document).scrollTop();
+        var elems = Object.keys(elementPositions);
+
+        for(var i = 0; i < elems.length; i++) {
+            if (elementPositions[elems[i]] < currentScrollPosition) {
+                $('.m-main-nav li a').removeClass('m-main-nav__highlighted');
+                var selector ="a[href='#" + elems[i] + "']";
+                $(selector).addClass('m-main-nav__highlighted');
+            }
+        }
+    });
+}
+
+function submitContactForm() {
+    $('.js-submit-form').click(function() {
+        var firstName = $('.js-form-first-name').val();
+        var lastName = $('.js-form-last-name').val();
+        var subject = $('.js-form-subject').val();
+        var text = $('.js-form-text').val();
     });
 }
